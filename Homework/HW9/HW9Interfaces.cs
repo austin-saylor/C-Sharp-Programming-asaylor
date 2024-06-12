21202697f27304d15e3377698e44395351036b3c
Austin Saylor <austin@saylors.com>
2024-06-12 13:36:14 -0600

    Homework 9 submission

---
+++ Homework/HW9/HW9Interfaces.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HW9
+{
+    interface IPlanet
+    {
+        // Setup methods for the 'IPlanet' interface.
+        void Name();
+        void Classification();
+        void Distance();
+    }
+
+    public class Mars : IPlanet
+    {
+        // Define the 'IPlanet' methods in the context of Mars
+        void IPlanet.Name()
+        {
+            Console.WriteLine("The name of the planet is 'Mars'.");
+        }
+
+        void IPlanet.Classification()
+        {
+            Console.WriteLine("The planet is classified as a Rocky Planet.");
+        }
+
+        void IPlanet.Distance()
+        {
+            Console.WriteLine("It is the 4th planet from the sun.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define an instance of IPlanet using Mars
+            IPlanet mars = new Mars();
+
+            // Call the methods of IPlanet using Mars
+            mars.Name();
+            mars.Classification();
+            mars.Distance();
+        }
+    }
+}
