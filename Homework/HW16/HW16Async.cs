260c53aa43877d2a3d8c6759f7ddaa602fdd1b7c
Austin Saylor <austin@saylors.com>
2024-06-19 14:22:59 -0600

    Homework 16 submission

--- Homework/HW16/HW16Async.cs
+++ Homework/HW16/HW16Async.cs
@@ -4,45 +4,66 @@ namespace HW16
 {
     public class HW16Async
     {
-        static Task PrintEvenNumbers()
+        static bool Prime(int num)
         {
+            // Method to check if the current number is prime
+            if (num <= 1)
+            {
+                return false;
+            }
+            
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+
+        static Task PrimeNumbers()
+        {
+            // Method to identify and print all prime numbers between 2 and 26
             return Task.Run(() =>
             {
-                for (int i = 0; i < 20; i++)
+                for (int i = 2; i < 26; i++)
                 {
-                    if (i % 2 == 0)
+                    if (Prime(i))
                     {
-                        Console.WriteLine(i);
+                        Console.WriteLine($"Prime Number:  {i}");
+                        Task.Delay(90).Wait();
                     }
-
-                    Task.Delay(100).Wait();
                 }
             });
         }
 
-        static Task PrintOddNumbers()
+        static Task CorrespondingLetters()
         {
+            // Method to identify prime numbers between 2 and 26, and translate them into their corresponding letters
             return Task.Run(() =>
             {
-                for (int i = 0; i < 20; i++)
+                for (int i = 2; i < 26; i++)
                 {
-                    if (i % 2 == 1)
+                    if (Prime(i))
                     {
-                        Console.WriteLine(i);
+                        char letter = (char)('A' + (i - 1) % 26);
+                        Console.WriteLine($"Corresponding Letter:  {letter}");
+                        Task.Delay(50).Wait();
                     }
-
-                    Task.Delay(50).Wait();
                 }
             });
         }
 
         static async Task Main(string[] args)
         {
-            Task printEven = PrintEvenNumbers();
-            Task printOdd = PrintOddNumbers();
+            // Start the program
+            Console.WriteLine("Async Demo:\n");
 
-            await printEven;
-            await printOdd;
+            // Define a task variable for each of the methods
+            Task Numbers = PrimeNumbers();
+            Task Letters = CorrespondingLetters();
+
+            // Execute the tasks asynchronously
+            await Numbers;
+            await Letters;
         }
     }
 }
